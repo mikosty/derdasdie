88f221a31c40aeeac00e1f2192e32c9d4beeebef
Mikko Markkinen <mikko.markkinen@gmail.com>
2017-01-07 12:30:43 +0200

    Search proto working

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import './App.css';
 import Searchbox from './Searchbox.js'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    const words = require('./words.json');
+    const trie = makeTrie(words);
+    this.state = {trie: trie};
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Der Die Das</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Resolve the gender of a German word
         </p>
-        <Searchbox />
+        <Searchbox trie={this.state.trie}/>
       </div>
 
     );
@@ -22,3 +30,27 @@ class App extends Component {
 }
 
 export default App;
+
+function makeTrie(words) {
+  var root = {};
+
+  for (var i = 0; i < words.length; ++i) {
+
+    var word = words[i];
+    var current = root;
+    var word_splitted = word.split(" ");
+    var rest = word_splitted[1];
+
+    var rest_a = rest.split("");
+
+    for (var j = 0; j < rest_a.length; ++j) {
+      var character = rest_a[j];
+      if (current[character] === undefined) {
+        current[character] = {};
+      }
+      current = current[character];
+    }
+    current["$"] = word;
+  }
+  return root;
+}
--- src/Searchbox.js
+++ src/Searchbox.js
@@ -7,17 +7,37 @@ class Searchbox extends React.Component {
     this.state = {
       value: '',
       results: [],
-      words: this.props.words,
+      trie: this.props.trie
     };
+    //console.log(this.state.trie);
 
     this.handleChange = this.handleChange.bind(this);
   }
+
+  findAllMatchingFromTrie(word) {
+    var word_a = word.split("");
+    word_a[0] = word_a[0].toUpperCase();
+    var current = this.state.trie;
+    for (var i = 0; i < word_a.length; ++i) {
+      var character = word_a[i];
+        if(current[character] === undefined) {
+          return [];
+        }
+        else {
+          current = current[character];
+        }
+    }
+    //this.setState({results: getResults(current)});
+    return getResults(current);
+  }
+
+
 
   handleChange(event) {
     this.setState({value: event.target.value});
     //alert('A name was submitted: ' + event.target.value);
         //event.preventDefault();
-    this.setState({results: getResults(this.state.words, event.target.value)});
+    this.setState({results: this.findAllMatchingFromTrie(event.target.value)});
   }
   render() {
     return (
@@ -28,7 +48,6 @@ class Searchbox extends React.Component {
             return <li>{listValue}</li>
           })}
         </ul>
-
       </div>
 
 
@@ -37,6 +56,29 @@ class Searchbox extends React.Component {
 }
 export default Searchbox;
 
-function getResults(words, word) {
-  return [word, word, word];
+function getResults(trie_part) {
+  var results = [];
+  const keys = getKeys(trie_part);
+
+  for (var i = 0; i < keys.length; ++i) {
+    var character = keys[i];
+      if(character === "$") {
+        results.push(trie_part[character]);
+      }
+      else {
+        results.push.apply(results, getResults(trie_part[character]));
+      }
+  }
+
+  return results;
+}
+
+function getKeys(hash) {
+  var keys = [];
+  for (var key in hash) {
+    if (hash.hasOwnProperty(key)) {
+    keys.push(key);
+    }
+  }
+  return keys;
 }
